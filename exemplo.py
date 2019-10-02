af73c6c9ae074f052e98d48ae630688d990c285b
nnathf <56075298+nnathf@users.noreply.github.com>
2019-10-02 10:03:22 -0300

    Add files via upload

---
+++ exemplo.py
@@ -0,0 +1,60 @@
+'''print('Insira o peso')  
+peso = float(input())
+print('Insira a altura')
+altura = float(input())
+imc = peso / (altura ** 2)
+print(imc)'''
+
+'''metros = float(input())
+centimetros = metros * 100
+print(centimetros)'''
+
+segundos = float(input())
+horas = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
